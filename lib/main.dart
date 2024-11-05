f6a546b96d250c823a6a9698bf928eb7426d7149
hyewonShin <67300338+hyewonShin@users.noreply.github.com>
2024-11-05 20:19:07 +0900

    refactor: startGame() 내부 코드 main()로 옮김

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,33 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:rpg_game/models/character.dart';
 import 'package:rpg_game/models/game.dart';
+import 'package:rpg_game/models/monster.dart';
 
 void main() async {
   Game game = Game();
-  game.loadCharacterStats();
-  game.loadMonsterStats();
+
+  stdout.write("👉🏻 캐릭터의 이름을 입력하세요: ");
+
+  String? heroName = stdin.readLineSync(encoding: Encoding.getByName('utf-8')!);
+
+  RegExp regex = RegExp(r'^[a-zA-Z가-힣]+$');
+  if (!regex.hasMatch(heroName ?? "")) {
+    print('캐릭터의 이름은 한글,영문 대소문자만 가능합니다 !');
+    return;
+  }
+  Character? heroData = await game.loadCharacterStats(heroName!);
+  await game.loadMonsterStats();
+  // print('heroData > $heroData');
+
+  print('⭐⭐⭐ 멋진 영웅 $heroName의 게임을 시작합니다 !⭐⭐⭐');
+  print(
+      '$heroName - 체력:${heroData?.heroHp} 공격력:${heroData?.heroAttack} 방어력:${heroData?.heroDefense}');
+  Monster randomMonster = game.getRandomMonster();
+
+  print('👀 두둥-! 새로운 몬스터가 나타났습니다 !');
+  print(
+      '${randomMonster.monsterName} - 체력: ${randomMonster.monsterHp}, 공격력: ${randomMonster.monsterAttack}');
+
+  // game.startGame();
 }
--- lib/models/game.dart
+++ lib/models/game.dart
@@ -63,31 +63,7 @@ class Game {
   }
 
   //게임을 시작하는 메서드
-  void startGame() async {
-    Game game = Game();
-
-    stdout.write("👉🏻 캐릭터의 이름을 입력하세요: ");
-
-    String? heroName =
-        stdin.readLineSync(encoding: Encoding.getByName('utf-8')!);
-
-    RegExp regex = RegExp(r'^[a-zA-Z가-힣]+$');
-    if (!regex.hasMatch(heroName ?? "")) {
-      print('캐릭터의 이름은 한글,영문 대소문자만 가능합니다 !');
-      return;
-    }
-    Character? heroData = await game.loadCharacterStats(heroName!);
-    await loadMonsterStats();
-    // print('heroData > $heroData');
-
-    print('⭐⭐⭐ 멋진 영웅 $heroName의 게임을 시작합니다 !⭐⭐⭐');
-    print(
-        '$heroName - 체력:${heroData?.heroHp} 공격력:${heroData?.heroAttack} 방어력:${heroData?.heroDefense}');
-    Monster randomMonster = getRandomMonster();
-    print('두둥-! 새로운 몬스터가 나타났습니다 !');
-    print(
-        '${randomMonster.monsterName} - 체력: ${randomMonster.monsterHp}, 공격력: ${randomMonster.monsterAttack}');
-  }
+  void startGame() async {}
 
   //전투를 진행하는 메서드
   void battle() {}
