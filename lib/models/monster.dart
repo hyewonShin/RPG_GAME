c699a7858f72289b730bbed080bde456b4942f58
hyewonShin <67300338+hyewonShin@users.noreply.github.com>
2024-11-05 23:18:47 +0900

    feat: monster 클래스에 attackCharacter() 생성

--- lib/models/monster.dart
+++ lib/models/monster.dart
@@ -8,14 +8,14 @@ class Monster {
   String monsterName = "";
 
   // 몬스터의 체력
-  int monsterHp = 0;
+  int monsterHp;
 
   // 랜덤으로 지정할 공격력 범위 최댓값
   //몬스터의 공격력은 캐릭터의 방어력보다 작을 수 없습니다. 랜덤으로 지정하여 캐릭터의 방어력과 랜덤 값 중 최대값으로 설정해주세요.
-  int monsterAttack = 0;
+  int monsterAttack;
 
   // 방어력(몬스터의 방어력은 0이라고 가정합니다.)
-  int monsterDefense = 0;
+  int monsterDefense;
 
   Monster(this.monsterName, this.monsterHp, this.monsterAttack)
       : monsterDefense = 0;
@@ -27,7 +27,16 @@ class Monster {
   // 공격 메서드
   //캐릭터에게 공격을 가하여 피해를 입힙니다.
   //캐릭터에게 입히는 데미지는 몬스터의 공격력에서 캐릭터의 방어력을 뺀 값이며, 최소 데미지는 0 이상입니다.
-  void attackCharacter(Character character) {}
+  void attackCharacter(Character character) {
+    print('$monsterName의 턴');
+    int demage = monsterAttack - character.heroDefense;
+
+    if (demage > 0) {
+      character.heroHp -= demage;
+      print(
+          '$monsterName이(가) ${character.heroName}에게 $monsterAttack의 데미지를 입혔습니다.');
+    }
+  }
 
   // 상태를 출력하는 메서드
   //몬스터의 현재 체력과 공격력을 매 턴마다 출력합니다.
