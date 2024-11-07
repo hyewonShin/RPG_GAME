afb7f8e8ff0659db297955e5928c7e288487e783
hyewonShin <67300338+hyewonShin@users.noreply.github.com>
2024-11-07 18:31:08 +0900

    refactor:  main()에 주석 추가

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,15 @@ import 'package:rpg_game/models/game.dart';
 
 Future<void> main() async {
   Game game = Game();
+
+  // await사용하여 명예의 전당 기능 가장 먼저 실행되도록 함.
   await game.hallOfFame();
 
+  // 사용자로부터 캐릭터 이름 입력받기 기능
   stdout.write("캐릭터의 이름을 입력하세요: ");
-
   String? heroName = stdin.readLineSync(encoding: Encoding.getByName('utf-8')!);
 
-  RegExp regex = RegExp(r'^[a-zA-Z가-힣]+$');
+  RegExp regex = RegExp(r'^[a-zA-Z가-힣]+$'); // 한글, 영문 대소문자만 허용
   if (heroName! == "") {
     print('캐릭터의 이름은 빈 문자열이 아니어야 합니다.');
     return;
@@ -19,5 +21,7 @@ Future<void> main() async {
     print('캐릭터의 이름은 한글,영문 대소문자만 가능합니다 !');
     return;
   }
+
+  // 캐릭터 이름 입력받은 이후 게임 시작
   game.startGame(heroName);
 }
